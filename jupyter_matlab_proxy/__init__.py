0dd14c25d27ecd877009cd96752be8ec5422ee5f
Callum M Walley <35017184+CallumWalley@users.noreply.github.com>
2021-03-23 15:56:17 +1300

    Update __init__.py

--- jupyter_matlab_proxy/__init__.py
+++ jupyter_matlab_proxy/__init__.py
@@ -8,6 +8,11 @@ def _get_env(port, base_url):
         "APP_PORT": str(port),
         "BASE_URL": f"{base_url}matlab",
         "APP_HOST": "127.0.0.1",
+        "MLM_LICENSE_FILE":"/opt/nesi/share/MATLAB/Licenses/apps.lic",
+        "PATH":"/opt/nesi/share/MATLAB/R2020a/bin:/opt/nesi/share/MATLAB/R2020a/etc/glnxa64",
+        "_JAVA_OPTIONS":"-Xmx256m",
+        "TZ":"Pacific/Auckland",
+        "MATLAB_LOG_DIR":"/tmp"
     }
 
 
@@ -18,7 +23,7 @@ def setup_matlab():
         "environment": _get_env,
         "absolute_url": True,
         "launcher_entry": {
-            "title": "MATLAB",
+            "title": "_MATLAB",
             "icon_path": os.path.join(
                 os.path.dirname(os.path.abspath(__file__)), "icons", "matlab.svg"
             ),
