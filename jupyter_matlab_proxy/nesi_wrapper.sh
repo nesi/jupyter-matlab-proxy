d22c10e4017a49bfc69b331b9d950ec9826dc08e
Callum Walley <callum.walley@nesi.org.nz>
2021-12-22 20:32:56 +1300

    moved testing to lic_test and added MLM_MOD_VER

--- jupyter_matlab_proxy/nesi_wrapper.sh
+++ jupyter_matlab_proxy/nesi_wrapper.sh
@@ -4,15 +4,6 @@ echo "Starting NeSI wrapper script"
 echo "BASE_URL: $BASE_URL"
 echo "APP_PORT: $APP_PORT"
 
-REQ_VER="46"
-
-module load MATLAB/2020b
-MLM_VER="$(/opt/nesi/nesi-apps-admin/Monitoring/bin/lmutil lmstat -c  $MLM_LICENSE_FILE -i MATLAB  | tail -n1 | awk '{print $2}')"
-if [[ ! "$MLM_VER" =~ ^[0-9]+$ || "$MLM_VER" -lt "$REQ_VER"  ]];then echo "Site licences for MATLAB too old. Unsetting licence path." && unset MLM_LICENSE_FILE ;fi
-
-# If called with TEST_SITE set, script will 0 if valid site licences, 1 if no.
-if [ -n "$TEST_SITE" ];then
-    [ -n  "$MLM_LICENSE_FILE" ] && exit 0 || exit 1
-fi
+module load MATLAB/$MLM_MOD_VER
 
 matlab-jupyter-app
