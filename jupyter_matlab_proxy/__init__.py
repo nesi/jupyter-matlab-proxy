45ceb72a3628f845887734892c996e82d6b6b95c
Callum Walley <callum.walley@nesi.org.nz>
2022-03-23 19:14:45 +1300

    FIX STUPID BUGS

--- jupyter_matlab_proxy/__init__.py
+++ jupyter_matlab_proxy/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from jupyter_matlab_proxy.jupyter_config import config
 import os
 
-matlab_module_version = "2021a"
+matlab_module_version = "2021b"
 
 
 def _get_env(port, base_url):
@@ -25,7 +25,7 @@ def _get_env(port, base_url):
         mwi_env.get_env_name_app_port(): str(port),
         mwi_env.get_env_name_base_url(): f"{base_url}matlab",
         mwi_env.get_env_name_app_host(): "127.0.0.1",
-        "MLM_MOD_VER": "2020b",
+        "MLM_MOD_VER": f"{matlab_module_version}",
     }
 
 
@@ -50,7 +50,7 @@ def setup_matlab():
     logger.debug(f"Extension Name: {config['extension_name']}")
     return {
         "command": [
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "nesi_wrapper.sh"),
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), "jupyter_matlab_proxy_wrapper_nesi.bash"),
         ],
         "timeout": 100,
         "environment": _get_env,
--- jupyter_matlab_proxy/jupyter_matlab_proxy_wrapper_nesi.bash
+++ jupyter_matlab_proxy/jupyter_matlab_proxy_wrapper_nesi.bash
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 
 echo "Starting NeSI wrapper script"
-echo "BASE_URL: $BASE_URL"
-echo "APP_PORT: $APP_PORT"
+echo "BASE_URL: ${BASE_URL}"
+echo "APP_PORT: ${APP_PORT}"
 
-module load MATLAB/$MLM_MOD_VER
+module load "MATLAB/${MLM_MOD_VER}"
 
 REQ_VER="46"
 
 # If called with DISALLOW_REMOTE_LIC set, script will 0 if valid site licences, 1 if no.
-if [ -n "$MLM_LICENSE_FILE" ]; then
-    LIC_VER="$(/opt/nesi/nesi-apps-admin/Monitoring/bin/lmutil lmstat -c $MLM_LICENSE_FILE -i MATLAB | tail -n1 | awk '{print $2}')"
-    if [[ ! "$LIC_VER" =~ ^[0-9]+$ || "$LIC_VER" -lt "$REQ_VER" ]]; then
+if [ -n "${MLM_LICENSE_FILE}" ]; then
+    LIC_VER="$(/opt/nesi/nesi-apps-admin/Monitoring/bin/lmutil lmstat -c ${MLM_LICENSE_FILE} -i MATLAB | tail -n1 | awk '{print $2}')"
+    if [[ ! "${LIC_VER}" =~ ^[0-9]+$ || "${LIC_VER}" -lt "${REQ_VER}" ]]; then
         echo "Site licences for MATLAB too old, unsetting MLM_LICENSE_FILE"
         unset MLM_LICENSE_FILE
     fi
